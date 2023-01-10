a1d60d8a882a98a462904e72967ce4a6dcd173df
nirmoru <nyxvermilion47@gmail.com>
2023-01-10 16:23:55 +0545

    Able to generate public key

--- Encryption.py
+++ Encryption.py
@@ -8,7 +8,7 @@ import os
 class AsymmetricEncryption:
 	def __init__(self, filename):
 		self.filename = filename
-	
+
 	@staticmethod
 	def RSAGenKey():
 		private_key = rsa.generate_private_key(
@@ -26,31 +26,26 @@ class AsymmetricEncryption:
 		)
 		with open(self.filename, 'wb') as pem_out:
 			pem_out.write(pem)
-			
+
 	def LoadKey(self):
 		with open(self.filename, 'rb') as pem_in:
 			pemlines = pem_in.read()
 		private_key = load_pem_private_key(pemlines, None, default_backend())
 		return private_key
 
-
-def GenRSA():
-	pk = AsymmetricEncryption.RSAGenKey()
-	dwd = os.getcwd()
-	try:
-		os.chdir('keys')
-	except FileNotFoundError:
-		os.mkdir('keys')
-		os.chdir('keys')
-	file = input('Enter the name of the file you want to create: ')
-	filename = dwd + '/keys/' + file + '.pem'
-	os.chdir(dwd)
-	savekey = AsymmetricEncryption(filename)
-	savekey.SaveKey(pk)
+	@staticmethod
+	def GenPublicKey(priv_filename):
+		public_key = priv_filename.public_key()
+		public_key_pem = public_key.public_bytes(
+			encoding=serialization.Encoding.PEM,
+			format=serialization.PublicFormat.SubjectPublicKeyInfo
+		)
+		with open('./keys/asd_pubkey.pub', 'wb') as f:
+			f.write(public_key_pem)
 
 
 def main():
-	GenRSA()
+	pass
 
 
 if __name__ == "__main__":
