67ba4d070a543374a97570f571d030ec613bd98f
nirmoru <nyxvermilion47@gmail.com>
2023-01-20 23:13:55 +0545

    Finished Symmetric Encryption with both modes (Auth and no auth), and the code should probably work

--- Encryption.py
+++ Encryption.py
@@ -1,8 +1,12 @@
 import base64
+import os
+from configparser import ConfigParser
 
 from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import padding
 from cryptography.hazmat.primitives import serialization, hashes
 from cryptography.hazmat.primitives.asymmetric import rsa, padding
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.primitives.serialization import load_pem_private_key, load_pem_public_key
 
 
@@ -106,9 +110,194 @@ class AsymmetricEncDecFile:
 		
 		if self.output is None:
 			out_filename = '{0}_dec.{1}'.format(str(self.filename).split('.')[0],
-												 str(self.filename).split('.')[1])
+												str(self.filename).split('.')[1])
 		else:
 			out_filename = self.output
 		
 		with open(out_filename, 'wb') as f:
 			f.write(dec)
+
+
+#### Symmetric Encryption From here ####
+class SymmetricEncryption:
+	def __init__(self, method, mode, auth=None):
+		self.method = method
+		self.mode = mode
+		self.auth = auth
+	
+	def EncryptWithoutAuth(self, data):
+		cipher = Cipher(self.method, self.mode)
+		encryptor = cipher.encryptor()
+		ct = encryptor.update(data) + encryptor.finalize()
+		return ct
+	
+	def DecryptWithoutAuth(self, data):
+		cipher = Cipher(self.method, self.mode)
+		decrypted = cipher.decryptor()
+		return decrypted.update(data) + decrypted.finalize()
+
+
+class SymmetricEncryptionWithAuth:
+	def __init__(self, method, mode, tag):
+		self.method = method
+		self.mode = mode
+		self.tag = tag
+	
+	def EncryptWithAuth(self, data):
+		if self.tag is None:
+			print("You need to set auth key.\n")
+			exit(-1)
+		cipher = Cipher(self.method, self.mode)
+		encryptor = cipher.encryptor()
+		encryptor.authenticate_additional_data(self.tag)
+		ct = encryptor.update(data) + encryptor.finalize()
+		return [ct, encryptor.tag]
+	
+	def DecryptWithAuth(self, data):
+		if self.tag is None:
+			print("You need to set auth key.\n")
+			exit(-1)
+		cipher = Cipher(self.method, self.mode)
+		decryptor = cipher.decryptor()
+		decryptor.authenticate_additional_data(self.tag)
+		pl = decryptor.update(data) + decryptor.finalize()
+		return pl
+
+
+class SymmetricPad:
+	def __init__(self, data):
+		self.data = data
+		self.size = 128
+	
+	def pad(self):
+		padder = padding.PKCS7(self.size).padder()
+		padded_data = padder.update(self.data)
+		padded_data += padder.finalize()
+		return padded_data
+	
+	def unpad(self):
+		padder = padding.PKCS7(self.size).unpadder()
+		unpadded_data = padder.update(self.data)
+		unpadded_data += padder.finalize()
+		return unpadded_data
+
+
+class SymmetricEncDecFileWithAuth:
+	def __init__(self, filename, auth_tag, output=None, config_file='cfg.ini'):
+		self.filename = filename
+		self.auth_tag = auth_tag.encode()
+		self.output = output
+		
+		self.key, self.iv = ReadConfigFile(config_file=config_file)
+		
+		self.method = algorithms.AES(self.key)
+	
+	def SymmetricEncFile(self):
+		mode = modes.GCM(self.iv)
+		with open(self.filename, 'rb') as f:
+			f_read = f.read()
+		
+		enc = SymmetricEncryptionWithAuth(method=self.method, mode=mode, tag=self.auth_tag)
+		enc_data, auth_tag_out = enc.EncryptWithAuth(f_read)
+		
+		if self.output is None:
+			out_filename = '{0}_SymEnc.{1}'.format(str(self.filename).split('/')[-1].split('.')[0],
+												   str(self.filename).split('/')[-1].split('.')[1])
+		else:
+			out_filename = self.output
+		
+		encrypted = base64.b64encode(enc_data)
+		enc_auth_tag = base64.b64encode(auth_tag_out)
+		
+		with open(out_filename, 'wb') as f:
+			f.write(encrypted)
+		
+		with open(out_filename + '.key', 'wb') as f:
+			f.write(enc_auth_tag)
+	
+	def SymmetricDecFile(self, key):
+		with open(self.filename, 'rb') as f:
+			undecoded_data = f.read()
+		
+		with open(key, 'rb') as f:
+			undecoded_key = f.read()
+		
+		decoded_data = base64.b64decode(undecoded_data)
+		decoded_key = base64.b64decode(undecoded_key)
+		mode = modes.GCM(self.iv, decoded_key)
+		
+		dec = SymmetricEncryptionWithAuth(method=self.method, mode=mode, tag=self.auth_tag)
+		plain_data = dec.DecryptWithAuth(data=decoded_data)
+		
+		with open(self.output, 'wb') as f:
+			f.write(plain_data)
+
+
+class SymmetricEncDecFileWithoutAuth:
+	def __init__(self, filename, output=None, config_file='cfg.ini'):
+		self.filename = filename
+		self.key, self.iv = ReadConfigFile(config_file=config_file)
+		self.output = output
+		
+		self.method = algorithms.AES(self.key)
+		self.mode = modes.CBC(self.iv)
+	
+	def SymmetricEncWithoutAuth(self):
+		with open(self.filename, 'rb') as f:
+			f_read = f.read()
+		
+		padded = SymmetricPad(f_read)
+		padded_data = padded.pad()
+		
+		cipher = SymmetricEncryption(method=self.method, mode=self.mode)
+		cipher_out = cipher.EncryptWithoutAuth(data=padded_data)
+		
+		with open(self.output, 'wb') as f:
+			encoded = base64.b64encode(cipher_out)
+			f.write(encoded)
+	
+	def SymmetricDecWithoutAuth(self):
+		with open(self.filename, 'rb') as fd:
+			f_read = fd.read()
+		
+		fd = base64.b64decode(f_read)
+		decipher = SymmetricEncryption(method=self.method, mode=self.mode)
+		decipher_out = decipher.DecryptWithoutAuth(data=fd)
+		
+		with open(self.output, 'wb') as f:
+			padded = SymmetricPad(decipher_out)
+			padded_data = padded.unpad()
+			f.write(padded_data)
+
+
+def GenerateConfigFile(config_file='cfg.ini'):
+	cfg = ConfigParser()
+	
+	try:
+		with open(config_file, encoding='utf-8') as f:
+			cfg.read_file(f)
+	except (FileNotFoundError, KeyError):
+		with open(config_file, 'w') as f:
+			f.write('[_]\nkey = 0\niv = 0')
+		GenerateConfigFile(config_file)
+	
+	cfg['_']['key'] = base64.b64encode(os.urandom(32)).decode()
+	cfg['_']['iv'] = base64.b64encode(os.urandom(16)).decode()
+	
+	with open(config_file, 'w', encoding='utf-8') as f:
+		cfg.write(f)
+
+
+def ReadConfigFile(config_file='cfg.ini'):
+	cfg = ConfigParser()
+	
+	try:
+		with open(config_file, encoding='utf-8') as f:
+			cfg.read_file(f)
+	except (FileNotFoundError, KeyError):
+		GenerateConfigFile(config_file)
+	
+	key = cfg['_']['key']
+	iv = cfg['_']['iv']
+	
+	return [base64.b64decode(key.encode()), base64.b64decode(iv.encode())]
